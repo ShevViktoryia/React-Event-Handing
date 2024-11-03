640f6908392f709567808ea40a80745c788a29ba
Victoria <shevvictoryia@gmail.com>
2024-11-03 23:01:12 +0300

    feat: add mouseOver

--- event-handing/src/App.tsx
+++ event-handing/src/App.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 function App() {
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
   return (
     <div className="container">
       <h1>Hello</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        onMouseOver={handleMouseOver}
+      >
+        Submit
+      </button>
     </div>
   );
 }
